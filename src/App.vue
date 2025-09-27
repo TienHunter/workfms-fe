bc189a0bc3424dd9e58a7919e1bbcf0470447fd4
TienHunter <tienkbtnhp@gmail.com>
2025-09-27 23:19:11 +0700

    sửa dockerfile và nginx.conf

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,29 @@
 <script setup>
-  import { useRoute } from "vue-router";
-  import { computed, onBeforeMount, onMounted, watchEffect, ref } from "vue";
-  import localStore from "./utils/localStore";
-  import { useI18n } from "vue-i18n";
-  import { ConfigProvider } from "ant-design-vue";
-  import enUS from "ant-design-vue/es/calendar/locale/en_US";
-  import viVn from "ant-design-vue/es/calendar/locale/vi_Vn";
-  const route = useRoute();
-  const { locale } = useI18n({ useScope: "global" });
-  const layout = computed(() => (route.meta?.layout || "default") + "-layout");
-  const language = ref("");
-  onBeforeMount(() => {
-    language.value = localStore.getItem("app_language");
-    if (!language.value) {
-      localStore.setItem("app_language", "vi");
-      language.value = "vi";
-    }
-  });
-  onMounted(() => {
-    if (language.value == "en") {
-      locale.value = "en";
-    } else {
-      locale.value = "vi";
-    }
-  });
+import { useRoute } from "vue-router";
+import { computed, onBeforeMount, onMounted, watchEffect, ref } from "vue";
+import localStore from "./utils/localStore";
+import { useI18n } from "vue-i18n";
+import { ConfigProvider } from "ant-design-vue";
+import enUS from "ant-design-vue/es/calendar/locale/en_US";
+import viVn from "ant-design-vue/es/calendar/locale/vi_VN";
+const route = useRoute();
+const { locale } = useI18n({ useScope: "global" });
+const layout = computed(() => (route.meta?.layout || "default") + "-layout");
+const language = ref("");
+onBeforeMount(() => {
+  language.value = localStore.getItem("app_language");
+  if (!language.value) {
+    localStore.setItem("app_language", "vi");
+    language.value = "vi";
+  }
+});
+onMounted(() => {
+  if (language.value == "en") {
+    locale.value = "en";
+  } else {
+    locale.value = "vi";
+  }
+});
 </script>
 
 <template>
